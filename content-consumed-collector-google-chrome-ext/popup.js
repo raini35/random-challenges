ab9740ebd6ec0f9cdf565c51600433312fe6d9f0
Rainier G <rainier.f.go@gmail.com>
2018-08-19 01:40:13 -0400

    Started on storing info in local storage

--- content-consumed-collector-google-chrome-ext/popup.js
+++ content-consumed-collector-google-chrome-ext/popup.js
@@ -7,15 +7,61 @@ let video_type = document.getElementById('video_type')
 function onClick(type) {
   chrome.runtime.getBackgroundPage(function(bg) {
     bg.type = type;
-    console.log("Type: " + bg.type); 
+    console.log("Type: " + bg.type);
   });
 }
 
 function loadData() {
   chrome.runtime.getBackgroundPage(function(bg) {
-      console.log(bg.title);
-      console.log(bg.type);
-      console.log(bg.url);
+      // old_items = []
+      // time_info = new Date();
+      // key = time_info.getMonth() + '-' + time_info.getDate();
+      //
+      // new_content = {
+      //   "title": bg.title,
+      //   "type": bg.type,
+      //   "url": bg.url,
+      //   "time_accessed": time_info
+      // };
+      //
+      // chrome.storage.sync.get("7-8", function(result) {
+      //   old_items = result;
+      // });
+      //
+      //
+      // if(old_items.length === 0) {
+      //   console.log('It is empty');
+      //   chrome.storage.sync.set({"7-8": "hello"}, function() {
+      //     chrome.storage.sync.get("7-8", function(result) {
+      //       console.log(result);
+      //     });
+      //   });
+      // } else {
+      //   console.log("There are things here");
+      // }
+
+      chrome.storage.sync.set({ "yourBody": "myBody" }, function(){
+          console.log("myBody placed");
+      });
+
+      chrome.storage.sync.get(/* String or Array */["yourBody"], function(items){
+          //  items = [ { "yourBody": "myBody" } ]
+          console.log(items); 
+      });
+
+      // chrome.storage.sync.get([key], function(result) {
+      //   console.log("Current: " + result);
+      // });
+      // else {
+      //
+      // }
+
+      //
+      //
+      //
+      // chrome.storage.sync.set({"today": }, function() {
+      //     console.log('Value is set to ' + bg.title);
+      // });
   });
 }
 

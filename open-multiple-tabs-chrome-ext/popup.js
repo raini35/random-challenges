080eff46939e95a483a31b63b3c2903ff9c8a229
Rainier G <rainier.f.go@gmail.com>
2018-09-01 23:42:39 -0500

    Put functions in popup.js to background.js

--- open-multiple-tabs-chrome-ext/popup.js
+++ open-multiple-tabs-chrome-ext/popup.js
@@ -1,43 +1,9 @@
-let onlyButton = document.getElementById('onlyButton');
-let urls = [
-  {
-    "url": "https://www.chase.com/",
-    "type": "login",
-    "username": "username",
-    "password": "password"
-  },
-  {
-    "url": "https://www.pnc.com/en/personal-banking.htm",
-    "type": "login",
-    "username": "username",
-    "password": "password"
-  },
-  {
-    "url": "https://www.wellsfargo.com/",
-    "type": "login",
-    "username": "username",
-    "password": "password"
-  }
-]
+document.getElementById("onlyButton").addEventListener("click", function() {
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  onlyButton.addEventListener('click',
-  openURL(urls));
-})
-
-function loginURL(url) {
-  username = url.username;
-  password = url.password;
-
-  chrome.tabs.create({url: url.url});
-}
-
-function openURL(urls) {
-  urls.map(url => {
-    if (url.type === "login") {
-      loginURL(url);
-    } else {
-      chrome.tabs.create({url: url.url})
-    }
+  chrome.runtime.sendMessage({
+    type: "openTab"
+  }, function(response) {
+    console.log("Sent request...");
   });
-}
+
+});

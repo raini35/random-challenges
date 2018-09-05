3766c4cb9ddb55cbd6c13e2f1cc03989582be3f1
Rainier G <rainier.f.go@gmail.com>
2018-09-04 21:46:32 -0500

    Playing around with geo-location

--- weather-dashboard/src/App.js
+++ weather-dashboard/src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import CityList from './components/CityList';
+import Demo from './components/Demo'; 
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityList: [],
+      availableCities: []
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Weather app</h1>
+        <CityList />
+        <button>+</button>
+        <Demo />
       </div>
     );
   }
---
+++ weather-dashboard/src/components/CityList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReatDOM from 'react-dom';
+
+function CityList (props) {
+  return <div>CityList</div>; 
+}
+
+export default CityList;

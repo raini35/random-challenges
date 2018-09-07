35887bc93c2809613c01d0252a868319f0469d5e
Rainier G <rainier.f.go@gmail.com>
2018-09-07 13:38:09 -0500

    Updated CurrentCity component

--- weather-dashboard/src/components/CurrentCity.js
+++ weather-dashboard/src/components/CurrentCity.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const CurrentCity = (props) => (
   <div>
-    <h1> Weather app </h1>
-    <img alt="weather icon" src="http://openweathermap.org/img/w/10d.png" />
-    <p>Name:</p>
-    <p>Description:</p>
-    <p>Current Temp:</p>
-    <p>Temp Min:</p>
-    <p>Temp Max:</p>
-    <p>Wind Speed:</p>
+    <h2> Current City </h2>
+    <img alt={props.data.weather.description + " icon" } src={"http://openweathermap.org/img/w/"+ props.data.weather.icon+".png"} />
+    <p>Name: {props.data.name}</p>
+    <p>Description: {props.data.weather.description}</p>
+    <p>Current Temp: {props.data.main.temp}</p>
+    <p>Temp Min: {props.data.main.temp_min}</p>
+    <p>Temp Max: {props.data.main.temp_max}</p>
+    <p>Wind Speed: {props.data.wind.speed}</p>
   </div>
 );
 

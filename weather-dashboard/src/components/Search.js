ef233352ebe78d58662a34f67f789d7573845324
Rainier G <rainier.f.go@gmail.com>
2018-09-09 23:54:59 -0500

    Updated Search component

--- weather-dashboard/src/components/Search.js
+++ weather-dashboard/src/components/Search.js
@@ -3,45 +3,26 @@ import React, { Component } from 'react'
 class Search extends Component {
  state = {
    query: '',
-   submitCity: this.props.submitCity
  }
 
- submitQuery = (event) => {
-   if(event.key === 'Enter') {
-     event.preventDefault();
-     this.state.submitCity(this.state.query)
-     this.setState({
-       query: ''
-     },() => {
-      this.search.value = '';
-    });
-  } else {
-    console.log(event.key);
-    console.log(this.search.value);
-    this.setState({
-        query: this.search.value
-      }, () => {
-        console.log(this.state.query);
-      })
-  }
+ handleInputChange = (event) => {
+   this.setState({
+     query: this.search.value
+   })
  }
 
- // handleInputChange = () => {
- //   console.log(this.search.value)
- //   // this.setState({
- //   //   query: this.search.value
- //   // }, () => {
- //   //   console.log(this.state.query);
- //   // })
- // }
+ handleSubmit = (event) => {
+   console.log(this.search.value)
+   event.preventDefault();
+ }
 
  render() {
    return (
-     <form>
+     <form onSubmit={e => { this.handleSubmit(e); }}>
        <input
          placeholder="Search for..."
          ref={input => this.search = input}
-         onKeyPress={this.submitQuery}
+         onChange={e => this.handleInputChange(e)}
        />
        <p>{this.state.query}</p>
      </form>
@@ -50,3 +31,57 @@ class Search extends Component {
 }
 
 export default Search
+
+
+// import React, { Component } from 'react'
+//
+// class Search extends Component {
+//  state = {
+//    query: '',
+//    submitCity: this.props.submitCity
+//  }
+//
+//  submitQuery = (event) => {
+//    if(event.key === 'Enter') {
+//      event.preventDefault();
+//      this.state.submitCity(this.state.query)
+//      this.setState({
+//        query: ''
+//      },() => {
+//       this.search.value = '';
+//     });
+//   } else {
+//     console.log(event.key);
+//     console.log(this.search.value);
+//     this.setState({
+//         query: this.search.value
+//       }, () => {
+//         console.log(this.state.query);
+//       })
+//   }
+//  }
+//
+//  // handleInputChange = () => {
+//  //   console.log(this.search.value)
+//  //   // this.setState({
+//  //   //   query: this.search.value
+//  //   // }, () => {
+//  //   //   console.log(this.state.query);
+//  //   // })
+//  // }
+//
+//  render() {
+//    return (
+//      <form>
+//        <input
+//          placeholder="Search for..."
+//          ref={input => this.search = input}
+//          onKeyPress={this.submitQuery}
+//        />
+//        <p>{this.state.query}</p>
+//      </form>
+//    )
+//  }
+// }
+//
+// export default Search

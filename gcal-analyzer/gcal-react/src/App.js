ecb7ff5c7038ef0c30f809fbc9e9634268ee2c45
Rainier G <rainier.f.go@gmail.com>
2018-10-03 23:53:04 -0500

    Added calendar to app

--- gcal-analyzer/gcal-react/src/App.js
+++ gcal-analyzer/gcal-react/src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Calendar from 'react-calendar';
 
 class App extends Component {
+  state = {
+    date: new Date(),
+  }
+
+  onChange = date => this.setState({ date })
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <p>{this.state.date.toString()}</p>
+        <Calendar
+          onChange={this.onChange}
+          value={this.state.date}
+        />
       </div>
     );
   }
